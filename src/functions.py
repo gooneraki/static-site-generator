f07087dcc81f7760824ad9bd278a4dab1b570c9d
gooneraki <hayiate4@hotmail.com>
2025-09-01 16:10:15 +0300

    Add text_to_textnodes function and corresponding unit tests

--- src/functions.py
+++ src/functions.py
@@ -111,3 +111,14 @@ def split_nodes_any(old_nodes, markdown_fn, new_text_type):
         new_nodes.extend(split_nodes)
 
     return new_nodes
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+
+    return nodes
